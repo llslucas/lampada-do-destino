ed7a45bb25adb8ea1b8b03d3ebc712a43b50a9f6
Lucas Souza <145349067+llslucas@users.noreply.github.com>
2025-05-23 23:53:12 -0300

    fixed nisus-overlay not showing

--- src/graphics/nisus-overlay.lua
+++ src/graphics/nisus-overlay.lua
@@ -10,8 +10,8 @@ function NisusOverlay:draw()
   love.graphics.setColor(0, 0, 0, 0.7)
   love.graphics.rectangle("fill", 0, 0, love.graphics.getWidth(), love.graphics.getHeight())
   love.graphics.setColor(1, 1, 1, 1)
-  local x = love.graphics.getWidth() / 2
-  local y = love.graphics.getHeight() / 2
+  local x = 320
+  local y = 320
   love.graphics.draw(self.img, x, y, 0, self.scale, self.scale, self.img:getWidth() / 2, self.img:getHeight() / 2)
 end
 
